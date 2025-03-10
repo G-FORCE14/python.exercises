3b08acb9011bcfcebc8464f5400b6c25b022f2e2
G-FORCE14 <theekamaug14@gmail.com>
2025-03-10 20:21:39 +0300

    Update exercise no8.py

--- exercise no8.py
+++ exercise no8.py
@@ -1,7 +1,11 @@
+#input from te user;
 num1=eval(input('Enter the first number:'))
 num2=eval(input('Enter the second number:'))
 num3=eval(input('Enter the third number:'))
+#get the sum of the input;
 total=sum([num1,num2,num3])
+#compute the average of the inputs;
 average=total/3
+#output the total and average;
 print('The total is:', total)
 print('The average is:', average)
